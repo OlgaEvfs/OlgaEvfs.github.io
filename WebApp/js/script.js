56b2cee70bae2e5f38620cb582835e9a525a516c
OlgaEvfs <olga.evstafieva@ivkhk.ee>
2025-09-16 15:23:24 +0300

    Начала писать калькулятор каллорий

--- WebApp/js/script.js
+++ WebApp/js/script.js
@@ -457,5 +457,59 @@ window.addEventListener('DOMContentLoaded', function() {
         function deleteNotDigits(str) {
             return +str.replace(/\D/g, '');
         }
-        
+
+        //Calculator
+
+        const result = document.querySelector('.calculator__result span');
+
+        let sex, height, weight, age, ratio;
+
+        if (this.localStorage.getItem('sex')) {
+            sex = localStorage.getItem('sex');
+        } else {
+            sex = 'female';
+            localStorage.setItem('sex', 'female');
+        }
+
+        if (this.localStorage.getItem('ratio')) {
+            ratio = localStorage.getItem('ratio');
+        } else {
+            ratio = 1.375;
+            localStorage.setItem('ratio', 1.375);
+        }
+        function calcTotal() {
+            if (!sex || !height || !weight || !age || !ratio) {
+                result.textContent = '____';
+                return;
+            }
+            if (sex === 'female') {
+                result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+            } else {
+                result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+            }
+        }
+
+        calcTotal();
+
+        function initLocalSettings(selector, activeClass) {
+            const elements = document.querySelectorAll(selector);
+
+            elements.forEach(elem => {
+                elem.classList.remove(activeClass);
+                if (elem.getAttribute('id') === localStorage.getItem('sex')) {
+                    elem.classList.add(activeClass);
+                }
+                if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+                    elem.classList.add(activeClass);
+                }
+            });
+        }
+
+        initLocalSettings('#gender div', 'calculating__choose-item_active');
+        initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active');
+
+        function getStaticInformation(selector, activeClass) {
+            const elements = document.querySelectorAll(selector);
+
+            
 });
